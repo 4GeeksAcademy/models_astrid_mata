b092eb9475e23e9e2d7024c383d4ba56f6bfe042
Astrid Mata <86080960+AstridMata@users.noreply.github.com>
2025-05-27 18:59:16 +0000

    clase 1

--- src/models.py
+++ src/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Table, Column, Integer, ForeignKey, relationship
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
@@ -7,13 +7,47 @@ db = SQLAlchemy()
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    password: Mapped[str] = mapped_column(String(120), nullable=False)
+   
+
+#relacion muchos a muchos con planey atravez de user_planet_favorite
+    favorite_planets: Mapped[list['Planets']]= relationship(
+        secondary=user_planet_favorite,
+        back_populates='user_who_favorited'
+    )
+
 
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "name": self.name
             # do not serialize the password, its a security breach
         }
+
+
+class Planets(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    climate: Mapped[str] = mapped_column(String(120), nullable=False)
+    
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate
+            
+        }
+
+
+#tabla de asociacion 
+user_planet_favorite= Table(
+    'user_planet_favorite', #nombre de la tabla
+    db.Model.metadata, 
+    Column('user_id', Integer, ForeignKey('user_id'), primary_key=True),
+    Column('planet_id', Integer, ForeignKey('planet_id'), primary_key=True)
+)
